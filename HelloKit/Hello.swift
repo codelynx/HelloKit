b4eed2ec6a266565515f75afa50b7b5c5ac98f0f
Kaz Yoshikawa <kaz@digitallynx.com>
2016-07-15 05:09:24 +0900

    added Hello.swift

---
+++ HelloKit/Hello.swift
@@ -0,0 +1,24 @@
+//
+//  Hello.swift
+//  Hello
+//
+//  Created by Kaz Yoshikawa on 7/15/16.
+//
+//
+
+import UIKit
+
+
+public class NSHello: NSObject {
+	public func hello() {
+		print("NSHello")
+	}
+}
+
+
+public class SwiftyHello {
+	public func hello() {
+		print("SwiftyHello")
+	}
+}
+
